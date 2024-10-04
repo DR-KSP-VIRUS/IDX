57df3f04eee783ab633c7eb7bfc22aeb0dd21958
KONJA KUMA SAMPSON <nkumasampson@gmail.com>
2024-10-04 11:07:06 +0000

    reduced inner padding

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <MainContent/>
     </main>
 
-    <footer class="bg-white py-6 flex place-items-center flex-col px-8 ">
+    <footer class="bg-white py-6 flex place-items-center flex-col px-4">
       <p class="rounded px-3 md:w-[820px] text-gray-400">&copy; 2024 IDX Withdrawal Channel. All Rights Reserved.</p>
     </footer>
   </div>
